caa5ebb905916b4a74fefbc13914ec458fcec1f3
Kiran0107--Krypto <kbs.jnv@gmail.com>
2024-11-21 10:02:22 +0530

    Kiran-Assignment5-Coin Denomination

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 def min_coins(coins, target_amount):
-    ""code here""
+    dp = [float('inf')] * (target_amount + 1)
+    dp[0] = 0
+    for coin in coins:
+        for amount in range(coin, target_amount + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    return dp[target_amount] if dp[target_amount] != float('inf') else -1
+
+'''
+coins = [1, 4, 6, 9, 14]
+target_amount = 26
+result = min_coins(coins, target_amount)
+print(result) '''
+
